39febf39a8818407cc699e783e84abee2b5a43a9
Daniela Klitzke <kochdaniela.historia@gmail.com>
2018-07-18 16:07:31 -0300

    Exercícios

---
+++ Fundamentos/CaixaEletronico/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEletronico
+{
+    class Cliente
+    {
+        public String nome;
+        public String rg;
+        public String endereco;
+        public int idade;
+
+        public bool EhMaiorDeIdade()
+        {
+            return this.idade >= 18;
+        }
+    }
+}
---
+++ Fundamentos/CaixaEletronico/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaixaEletronico
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Conta conta = new Conta();
+            Cliente cliente = new Cliente();
+            conta.cliente = cliente;
+
+            cliente.nome = "Victor";
+            conta.cliente.rg = "12345678-9";
+
+            MessageBox.Show(conta.cliente.nome);
+
+
+        }
+    }
+}
