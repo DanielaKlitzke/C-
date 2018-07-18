498e6dc4571df1a939157a0450ff2ebdc38551f9
Daniela Klitzke <kochdaniela.historia@gmail.com>
2018-07-18 16:17:12 -0300

    classes abstratas

---
+++ Fundamentos/classsesAbstratas/Cliente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classsesAbstratas
+{
+    class Cliente
+    {
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public bool Emancipado { get; set; }
+        public string rg;
+        public string endereco;
+        public int Idade { get; set; }
+
+        public bool podeAbrirConta()
+        {
+            var maiorDeIdade = (this.Idade >= 18);
+            var emancipado = (this.Emancipado);
+            var temCpf = !string.IsNullOrEmpty(this.Cpf);
+
+            return (maiorDeIdade || emancipado) && temCpf;
+        }
+    }
+}
---
+++ Fundamentos/classsesAbstratas/ContaPoupanca.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classsesAbstratas
+{
+    class ContaPoupanca : Conta
+    {
+        public override void Saca(double valor)
+        {
+            this.Saldo -= valor + 0.1;
+        }
+
+        public void CalculaInvestimento()
+        {
+
+        }
+    }
+}
